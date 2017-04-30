ae7278449055f392a03a9ffccb066a69edca2cf5
Unknown <me@rodogir.com>
2017-05-01 01:55:35 +0200

    heroku integration

--- src/server/index.ts
+++ src/server/index.ts
@@ -2,8 +2,11 @@ import * as express from 'express';
 
 const app = express();
 app.disable('x-powered-by');
+app.get('/', (req, res) => {
+  res.send('hello world');
+})
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 const listener = app.listen(port, () =>
   console.log(`Server listening on port ${port}`),
 );
