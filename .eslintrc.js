480a014d7875fd97987f89cca5327cb353e86116
Unknown <me@rodogir.com>
2017-05-01 21:36:40 +0200

    eslint for all the .js files

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+const eslintcfg = {
+  "extends": [
+    "airbnb-base"
+  ],
+  "env": {
+    "es6": true,
+    "node": true,
+  },
+  "rules": {
+    quotes: ["error", "double"],
+  }
+};
+
+module.exports = eslintcfg;
