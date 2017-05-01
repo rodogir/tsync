9773dd54327df2547005ccfe2251206e0de311cb
Unknown <me@rodogir.com>
2017-05-01 15:22:41 +0200

    wip setup
    lf instead of crlf, tslint

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,11 +1,11 @@
-import * as express from 'express';
-import renderHTML from './renderHTML';
+import * as express from "express";
+import renderHTML from "./renderHTML";
 
 const app = express();
-app.disable('x-powered-by');
-app.get('/', (req, res) => {
+app.disable("x-powered-by");
+app.get("/", (req, res) => {
   res.send(renderHTML());
-})
+});
 
 const port = process.env.PORT || 3000;
 const listener = app.listen(port, () =>
--- src/server/renderHTML.tsx
+++ src/server/renderHTML.tsx
@@ -1,6 +1,6 @@
-import * as React from 'react';
-import { renderToStaticMarkup } from 'react-dom/server';
-import HTML from './HTML';
+import * as React from "react";
+import { renderToStaticMarkup } from "react-dom/server";
+import HTML from "./HTML";
 
 function renderHTML(): string {
   return renderToStaticMarkup(<HTML />);
