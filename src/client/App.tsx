007b754edd962f08452659d2da09b18df7f9661a
Unknown <me@rodogir.com>
2017-05-07 22:49:09 +0200

    move page meta from html to app code

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,9 +1,16 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import { Helmet } from "react-helmet";
 
 function App() {
   return (
     <article>
+      <Helmet>
+        <html lang="en"/>
+        <meta charSet="utf-8" />
+        <title>⚡🕗 TSYNC</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Helmet>
       👋 Hello World <br />
       🚧 tsync is being created here
     </article>);
