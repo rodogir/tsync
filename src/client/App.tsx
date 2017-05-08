a07fb189fa03d46a3a23d29cd41a46a22e714403
Unknown <me@rodogir.com>
2017-05-08 23:44:41 +0200

    add react router

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,19 +1,30 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { Helmet } from "react-helmet";
+import { Link, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <article>
+    <div>
       <Helmet>
-        <html lang="en"/>
+        <html lang="en" />
         <meta charSet="utf-8" />
         <title>⚡🕗 TSYNC</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Helmet>
-      👋 Hello World <br />
-      🚧 tsync is being created here
-    </article>);
+      <Switch>
+        <Route exact path="/" render={() => {
+          return (
+            <div>
+              👋 Hello World <br />
+              🚧 tsync is being created here
+              <Link to="/login">Login</Link>
+            </div>
+          );
+        }} />
+        <Route exact path="/login" render={() => <div>🚧 log in here soon</div>} />
+      </Switch>
+    </div>);
 }
 
 export default App;
--- src/client/index.tsx
+++ src/client/index.tsx
@@ -1,12 +1,15 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { AppContainer } from "react-hot-loader";
+import { BrowserRouter as Router } from 'react-router-dom'
 import App from "./App";
 
 const render = (Component: () => React.ReactElement<any>) => {
   ReactDOM.render(
     <AppContainer>
-      <Component />
+      <Router>
+        <Component />
+      </Router>
     </AppContainer>,
     document.getElementById("app"),
   );
