9bb3adfbcc6f4233807547930b4d6b172782ac86
Unknown <me@rodogir.com>
2017-05-19 01:02:19 +0200

    remove unneeded import

--- src/client/core/session/epic.ts
+++ src/client/core/session/epic.ts
@@ -3,7 +3,6 @@ import { ActionsObservable, combineEpics } from "redux-observable";
 import "rxjs/add/observable/fromPromise";
 import "rxjs/add/observable/of";
 import "rxjs/add/operator/catch";
-import "rxjs/add/operator/filter";
 import "rxjs/add/operator/map";
 import "rxjs/add/operator/mergeMap";
 import { Observable } from "rxjs/Observable";
