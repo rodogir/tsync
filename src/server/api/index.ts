dc7a264fb170980d490bb18466cd2599e04069b1
Unknown <me@rodogir.com>
2017-05-20 15:44:21 +0200

    add toggl proxy

--- src/server/api/index.ts
+++ src/server/api/index.ts
@@ -1,9 +1,17 @@
 import * as express from "express";
+import * as proxy from "express-http-proxy";
 import authentication from "./authentication";
 
 const app = express();
 
 app.use(authentication);
+
+app.use("/toggle", proxy("www.toggle.com", {
+  https: true,
+  proxyReqPathResolver: (req: express.Request) => {
+    return req.path;
+  },
+}));
 
 app.get("/verify-token", (req, res) => {
   res.status(200).send();
